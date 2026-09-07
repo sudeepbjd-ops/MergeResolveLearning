f5a3d5b29c9a443defa5f0e5c012eecb7b9a92e9
sudeepbjd-ops <sudeepbjd@gmail.com>
2026-09-06 21:26:26 -0400

    Sudeep1 (#5)
    * Raj updates README
    
    * Sudeep updates README
    
    * hghhgghf
    
    * fdfsdf
    
    * hghhgghf
    
    * hghgfg
    
    * hghgfg
    
    ---------
    
    Co-authored-by: sudee <sudeeprky@gmail.com>

--- src/main/java/com/sudeep/development/controller/HelloController.java
+++ src/main/java/com/sudeep/development/controller/HelloController.java
@@ -9,9 +9,9 @@ public class HelloController {
     @GetMapping("/")
     public String home() {
 
-        String x;
-        String y;
-        callSun(x,y);
-        return "sudeep dev branch"
+        int a,b;
+        mul(a,b);
+        return "I am changing in Sudeep1 branch";
         
     }
+}
